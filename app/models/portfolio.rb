f9b127fdb8654cca3bcb68a319e2009d020d461f
Kristina <kristina@Kristinas-Mac-mini.local>
2019-09-11 14:16:28 -0600

    integrated technologies model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,8 @@
 class Portfolio < ApplicationRecord
+    #establishing connection with db technology table
+    has_many :technologies
+    #getting access to Placeholder method from concerns/placeholder.r
+    include Placeholder
     #data validation- a portfolio must have a title, body, main_image, thumb_image in order to be created.
     validates_presence_of :title, :body, :main_image, :thumb_image
 
@@ -7,8 +11,8 @@ class Portfolio < ApplicationRecord
 
     #||= Conditional. Only sets a default image if it DOES NOT already exist. 
     def set_defaults
-        self.main_image ||= "https://placehold.it/600x400"
-        self.thumb_image ||= "https://placehold.it/350x200"
+        self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+        self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
     end
 
 end
