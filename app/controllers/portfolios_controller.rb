65889f911dc1ef7d2bac043a86a56c73e767e326
Kristina <kristinakfriel@gmail.com>
2019-09-13 14:58:51 -0600

    refactored strong params for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -22,7 +22,7 @@ class PortfoliosController < ApplicationController
     #create action creates a portfolio item
     def create
         #specifying the parameters that are allowed to go through the system.
-        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :substring, :body, technologies_attributes: [:name]))
+        @portfolio_item = Portfolio.new(portfolio_params)
     
         respond_to do |format|
             if @portfolio_item.save
@@ -47,7 +47,7 @@ class PortfoliosController < ApplicationController
     def update
         @portfolio_item = Portfolio.find(params[:id])
         respond_to do |format|
-            if @portfolio_item.update(params.require(:portfolio).permit(:title, :substring, :body))
+            if @portfolio_item.update(portfolio_params)
                 format.html { redirect_to portfolios_path, notice: 'Blog was successfully updated.' }
                 format.json { render :show, status: :ok, location: @blog }
             else
@@ -74,6 +74,17 @@ class PortfoliosController < ApplicationController
             format.html { redirect_to portfolios_url, notice: 'Record was deleted' }
         end
     end
+
+        #private is a way to organize our methods. private methods can only be used inside this specific class.
+        private
+        def portfolio_params
+            params.require(:portfolio).permit(:title,
+                                              :substring,
+                                              :body,
+                                              technologies_attributes: [:name]
+                                             )
+
+        end
 
 
 
