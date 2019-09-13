0af72bfe73ee3ae38522d959aeb5e735c4b83411
Kristina <kristinakfriel@gmail.com>
2019-09-13 12:41:23 -0600

    Completed auth with virtual attrs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  #checks if a name is supplied before proceeding to these methods.
+  validates_presence_of :name
+  def first_name
+    #self = specfic user. split = takes a string and splits it into array.
+    self.name.split.first
+  end
+
+  def last_name
+    self.name.split.last
+  end
+
 end
