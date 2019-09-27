fcbc581cc17b2fec317255e5c28c4d4edfb8799f
Kristina <kristinakfriel@gmail.com>
2019-09-27 16:46:42 -0600

    implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,54 @@
 #This is a view helper, it helps with multiple views and prevents repeative code.
 
 module ApplicationHelper
-    # If the current user is coming from the user model as guest, show register and login button otherwise show logout button
-    def login_helper style = ''
-        if current_user.is_a?(GuestUser)
-            (link_to "Register", new_user_registration_path, class: style) +
-            "".html_safe +
-            (link_to "Login", new_user_session_path, class: style)
-        else
-            link_to "Logout", destroy_user_session_path, method: :delete, class: style
-        end
+  # If the current user is coming from the user model as guest, show register and login button otherwise show logout button
+  def login_helper style = ''
+    if current_user.is_a?(GuestUser)
+      (link_to "Register", new_user_registration_path, class: style) +
+      "".html_safe +
+      (link_to "Login", new_user_session_path, class: style)
+    else
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
+  end
+
+  def nav_items 
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_path,
+        title: 'About'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      },
+    ]
+  end
+
+  #style argument is for css styles. tag_type argument is the type of html tag. 
+  # helps make a diverse nav bar for multiple pages.
+  def nav_helper style, tag_type
+    nav_links = ''
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+    nav_links.html_safe
+  end
+
+  def active? path 
+    "active" if current_page? path
+  end
 
 end
