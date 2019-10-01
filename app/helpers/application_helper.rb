85cfc71c58ba7d3d18c02a82e6f763b13a5d06bc
Kristina <kristinakfriel@gmail.com>
2019-10-01 16:59:24 -0600

    refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -51,4 +51,21 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts 
+    alert = (flash[:alert] || flash[:error] || flash[:notice] )
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Kristina Hales Portfolio", sticky: false) 
+  end
+
+
+
+
+
+
+
 end
