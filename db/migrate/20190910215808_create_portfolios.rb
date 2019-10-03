d36787eb089a2b58bcb063914cdf4c47c22ba04c
Kristina <kristinakfriel@gmail.com>
2019-10-03 12:35:19 -0600

    updated table

--- db/migrate/20190910215808_create_portfolios.rb
+++ db/migrate/20190910215808_create_portfolios.rb
@@ -2,7 +2,7 @@ class CreatePortfolios < ActiveRecord::Migration[5.0]
   def change
     create_table :portfolios do |t|
       t.string :title
-      t.string :substring
+      t.string :subtitle
       t.string :string
       t.string :body
       t.string :text
